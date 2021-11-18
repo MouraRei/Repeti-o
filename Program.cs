b5f8fb7e05d8b8d7dffe6b36a0b572b3be9172b2
MouraRei <reinaldomoura.contato@outlook.com>
2021-11-18 15:30:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Calculando Poupança");
+
+double valorInvestido = 1000;
+//0.36% = 0.0036
+int contadorMes = 1;
+
+while (contadorMes <= 12)
+{
+    valorInvestido = valorInvestido + valorInvestido * 0.0036;
+    Console.WriteLine("Após " + contadorMes + "mês, você terá R$ " + valorInvestido);
+    contadorMes += 1;
+}
+
+
+
+Console.ReadLine();
